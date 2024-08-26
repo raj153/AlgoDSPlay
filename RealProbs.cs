384719523ecc65d4c7d3501c8333f1f0f8ada423
Pallavi <krpallavi211@gmail.com>
2024-08-26 05:51:47 +0530

    Upd!

--- RealProbs.cs
+++ RealProbs.cs
@@ -205,5 +205,80 @@ namespace AlgoDSPlay
                 this.End = end;
             }
         }
+        //https://www.algoexpert.io/questions/search-for-range
+          // O(log(n)) time | O(log(n)) space
+        public static int[] SearchForRangeNonOptimal(int[] array, int target) {
+            int[] finalRange = { -1, -1 };
+            alteredBinarySearchNonOptimal(array, target, 0, array.Length - 1, finalRange, true);
+            alteredBinarySearchNonOptimal(array, target, 0, array.Length - 1, finalRange, false);
+            return finalRange;
+        }
+
+        public static void alteredBinarySearchNonOptimal(
+            int[] array, int target, int left, int right, int[] finalRange, bool goLeft
+        ) {
+            if (left > right) {
+            return;
+            }
+            int mid = (left + right) / 2;
+            if (array[mid] < target) {
+            alteredBinarySearchNonOptimal(array, target, mid + 1, right, finalRange, goLeft);
+            } else if (array[mid] > target) {
+            alteredBinarySearchNonOptimal(array, target, left, mid - 1, finalRange, goLeft);
+            } else {
+            if (goLeft) {
+                if (mid == 0 || array[mid - 1] != target) {
+                finalRange[0] = mid;
+                } else {
+                alteredBinarySearchNonOptimal(array, target, left, mid - 1, finalRange, goLeft);
+                }
+            } else {
+                if (mid == array.Length - 1 || array[mid + 1] != target) {
+                finalRange[1] = mid;
+                } else {
+                alteredBinarySearchNonOptimal(
+                    array, target, mid + 1, right, finalRange, goLeft
+                );
+                }
+            }
+            }
+        }
+
+        // O(log(n)) time | O(1) space
+        public static int[] SearchForRangeOptimal(int[] array, int target) {
+            int[] finalRange = { -1, -1 };
+            alteredBinarySearch(array, target, 0, array.Length - 1, finalRange, true);
+            alteredBinarySearch(array, target, 0, array.Length - 1, finalRange, false);
+            return finalRange;
+        }
+
+        public static void alteredBinarySearchOptimal(
+            int[] array, int target, int left, int right, int[] finalRange, bool goLeft
+        ) {
+            while (left <= right) {
+            int mid = (left + right) / 2;
+            if (array[mid] < target) {
+                left = mid + 1;
+            } else if (array[mid] > target) {
+                right = mid - 1;
+            } else {
+                if (goLeft) {
+                if (mid == 0 || array[mid - 1] != target) {
+                    finalRange[0] = mid;
+                    return;
+                } else {
+                    right = mid - 1;
+                }
+                } else {
+                if (mid == array.Length - 1 || array[mid + 1] != target) {
+                    finalRange[1] = mid;
+                    return;
+                } else {
+                    left = mid + 1;
+                }
+                }
+            }
+            }
+        }
     }
 }
--- Subsequence.cs
+++ Subsequence.cs
@@ -33,6 +33,108 @@ namespace AlgoDSPlay
             }
             return maxSoFar;
         }
+        //https://www.algoexpert.io/questions/max-subset-sum-no-adjacent
+        public static int MaxSubsetSumNoAdjacent(int[] array) {
+            // T:O(n) time | S:O(n) space
+            int result = MaxSubsetSumNoAdjacentNonOptimal(array);
+            // T:O(n) time | S:O(1) space
+
+            result = MaxSubsetSumNoAdjacentOptimal(array);
+            return result;
+        }
+        public static int MaxSubsetSumNoAdjacentOptimal(int[] array) {
+            if (array.Length == 0) {
+            return 0;
+            } else if (array.Length == 1) {
+            return array[0];
+            }
+            int second = array[0];
+            int first = Math.Max(array[0], array[1]);
+            for (int i = 2; i < array.Length; i++) {
+            int current = Math.Max(first, second + array[i]);
+            second = first;
+            first = current;
+            }
+            return first;
+        }
+         public static int MaxSubsetSumNoAdjacentNonOptimal(int[] array) {
+            // T:O(n) time | S:O(n) space
+            if (array.Length == 0) {
+            return 0;
+            } else if (array.Length == 1) {
+            return array[0];
+            }
+            int[] maxSums = (int[])array.Clone();
+            maxSums[1] = Math.Max(array[0], array[1]);
+            for (int i = 2; i < array.Length; i++) {
+            maxSums[i] = Math.Max(maxSums[i - 1], maxSums[i - 2] + array[i]);
+            }
+            return maxSums[array.Length - 1];
+        }
+        //https://www.algoexpert.io/questions/four-number-sum
+
+        public static List<int[]> FourNumberSum(int[] array, int targetSum) {
+         // Average: O(n^2) time | O(n^2) space
+        // Worst: O(n^3) time | O(n^2) space
+    Dictionary<int, List<int[]> > allPairSums =
+      new Dictionary<int, List<int[]> >();
+    List<int[]> quadruplets = new List<int[]>();
+    for (int i = 1; i < array.Length - 1; i++) {
+      for (int j = i + 1; j < array.Length; j++) {
+        int currentSum = array[i] + array[j];
+        int difference = targetSum - currentSum;
+        if (allPairSums.ContainsKey(difference)) {
+          foreach (int[] pair in allPairSums[difference]) {
+            int[] newQuadruplet = { pair[0], pair[1], array[i], array[j] };
+            quadruplets.Add(newQuadruplet);
+          }
+        }
+      }
+      for (int k = 0; k < i; k++) {
+        int currentSum = array[i] + array[k];
+        int[] pair = { array[k], array[i] };
+        if (!allPairSums.ContainsKey(currentSum)) {
+          List<int[]> pairGroup = new List<int[]>();
+          pairGroup.Add(pair);
+          allPairSums.Add(currentSum, pairGroup);
+        } else {
+          allPairSums[currentSum].Add(pair);
+        }
+      }
+    }
+    return quadruplets;
+  }
+    //https://www.algoexpert.io/questions/longest-peak
+    public static int LongestPeak(int[] array) {
+          // O(n) time | O(1) space - where n is the length of the input array
+    int longestPeakLength = 0;
+    int i = 1;
+    while (i < array.Length - 1) {
+      bool isPeak = array[i - 1] < array[i] && array[i] > array[i + 1];
+      if (!isPeak) {
+        i += 1;
+        continue;
+      }
+
+      int leftIdx = i - 2;
+      while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]) {
+        leftIdx -= 1;
+      }
+
+      int rightIdx = i + 2;
+      while (rightIdx < array.Length && array[rightIdx] < array[rightIdx - 1]) {
+        rightIdx += 1;
+      }
+      int currentPeakLength = rightIdx - leftIdx - 1;
+      if (currentPeakLength > longestPeakLength) {
+        longestPeakLength = currentPeakLength;
+      }
+      i = rightIdx;
+    }
+    return longestPeakLength;
+  }
+        
+
 
     }
 }
